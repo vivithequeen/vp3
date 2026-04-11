76030fff75e44d94dd690e741f4763a5530d0efd
vivithequeen1 <vivithequeen1@gmail.com>
2026-04-11 10:08:38 -0400

    feat: add table demo code to build off of

--- app.go
+++ app.go
@@ -1,14 +1,2 @@
 package main
 
-import (
-    tea "github.com/charmbracelet/bubbletea"
-)
-
-func main() {
-    p := tea.NewProgram(
-        newSimplePage("This app is under construction"),
-    )
-    if err := p.Start(); err != nil {
-        panic(err)
-    }
-}
