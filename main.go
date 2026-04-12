69fd024b2e7c7be48c384be9f6ed214e9752233a
vivithequeen1 <vivithequeen1@gmail.com>
2026-04-12 13:35:10 -0400

    feat: add progress bar

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
+	"time"
+
+	"charm.land/bubbles/v2/progress"
 	"charm.land/bubbles/v2/table"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
@@ -19,18 +23,96 @@ import (
 	"github.com/qeesung/image2ascii/convert"
 )
 
+var (
+	helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
+	yellow    = lipgloss.Color("#FDFF8C")
+	pink      = lipgloss.Color("#FF7CCB")
+)
+
 var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
 var currentStreamer beep.StreamCloser
 
+type tickMsg struct{}
 type model struct {
 	table    table.Model
 	albumArt string
+
+	percent  float64
+	progress progress.Model
 }
 
-func (m model) Init() tea.Cmd { return nil }
+func (m model) Init() tea.Cmd { return tickCmd() }
+
+func createTable() table.Model {
+	columns := []table.Column{
+		{Title: "#", Width: 3},
+		{Title: "Title", Width: 18},
+		{Title: "Artist", Width: 18},
+		{Title: "Albumn", Width: 18},
+		{Title: "Filepath", Width: 0},
+	}
+	var rows []table.Row
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	musicDir := filepath.Join(homeDir, "Music")
+	index := 0
+	filepath.Walk(musicDir, func(fp string, fi os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		if fi.IsDir() {
+			return nil
+		}
+		matched, err := filepath.Match("*.mp3", fi.Name())
+		if err != nil {
+			return err
+		}
+		if matched {
+			f, err := os.Open(fp)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			m, err := tag.ReadFrom(f)
+			f.Close()
+
+			if err != nil {
+				return nil
+			}
+			index++
+			rows = append(rows, table.Row{strconv.Itoa(index), m.Title(), m.Artist(), m.Album(), fp})
+		}
+		return nil
+	})
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(20),
+		table.WithWidth(64),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+	return t
+}
 
 func SwapMusicTo(fp string) {
 	f, err := os.Open(fp)
@@ -55,9 +137,20 @@ func SwapMusicTo(fp string) {
 	resampled := beep.Resample(4, format.SampleRate, beep.SampleRate(44100), streamer)
 	speaker.Play(resampled)
 }
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second/15, func(t time.Time) tea.Msg {
+		return tickMsg{}
+	})
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	case tickMsg:
+		m.percent += 0.1
+
+		return m, tickCmd()
 	case tea.KeyPressMsg:
 		switch msg.String() {
 		case "esc":
@@ -91,15 +184,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if err != nil {
 				break
 			}
-
+			size := 15
 			convertOptions := convert.DefaultOptions
-			convertOptions.FixedWidth = 25
-			convertOptions.FixedHeight = 10
+			convertOptions.FixedWidth = int(float64(size) * 2.5)
+			convertOptions.FixedHeight = size
 
 			converter := convert.NewImageConverter()
 			s := converter.Image2ASCIIString(img, &convertOptions)
 
-			s += "\n" + tags.Title()
+			s += "\n" + tags.Title() + " - " + tags.Artist()
 			m.albumArt = s
 			go SwapMusicTo(m.table.SelectedRow()[4])
 			return m, nil
@@ -113,7 +206,7 @@ func (m model) View() tea.View {
 	tableView := baseStyle.Render(m.table.View())
 	buffer := strings.Repeat("\n     ", 20)
 	content := lipgloss.JoinHorizontal(lipgloss.Top,
-		tableView, buffer, m.albumArt)
+		tableView, buffer, m.albumArt+"\n"+m.progress.ViewAs(m.percent))
 	return tea.NewView(content + "\n  " + m.table.HelpView() +
 		"\n")
 }
@@ -122,70 +215,10 @@ func main() {
 
 	speaker.Init(beep.SampleRate(44100), 4410)
 
-	columns := []table.Column{
-		{Title: "#", Width: 3},
-		{Title: "Title", Width: 18},
-		{Title: "Artist", Width: 18},
-		{Title: "Albumn", Width: 18},
-		{Title: "Filepath", Width: 0},
-	}
-	var rows []table.Row
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Println(err)
-	}
+	t := createTable()
 
-	musicDir := filepath.Join(homeDir, "Music")
-	filepath.Walk(musicDir, func(fp string, fi os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Println(err)
-			return nil
-		}
-		if fi.IsDir() {
-			return nil
-		}
-		matched, err := filepath.Match("*.mp3", fi.Name())
-		if err != nil {
-			return err
-		}
-		if matched {
-			f, err := os.Open(fp)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			m, err := tag.ReadFrom(f)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			rows = append(rows, table.Row{"1", m.Title(), m.Artist(), m.Album(), fp})
-		}
-		return nil
-	})
-
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(20),
-		table.WithWidth(64),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	t.SetStyles(s)
-
-	m := model{table: t}
+	prog := progress.New(progress.WithScaled(true), progress.WithColors(pink, yellow))
+	m := model{table: t, progress: prog}
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
